c76dc1ed9f7824ef8b5ffbf07fa01db693e4fbf8
Ethan Morris <ethan.morris33@gmail.com>
2022-06-17 21:08:17 -0500

    add select menu on stats page

--- pages/stats.js
+++ pages/stats.js
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { Fragment, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
 import { useTable, usePagination, useSortBy, useFlexLayout } from 'react-table';
 
 function Table({ columns, data }) {
@@ -37,7 +41,7 @@ function Table({ columns, data }) {
 	// Render the UI for your table
 	return (
 		<>
-			<div className="wrapper overflow-x-auto">
+			<div className="wrapper overflow-x-auto bg-white border rounded-md shadow-sm p-4">
 				<table
 					{...getTableProps()}
 					className="min-w-full text-sm divide-y divide-gray-200"
@@ -49,7 +53,7 @@ function Table({ columns, data }) {
 									<th
 										key={index}
 										{...column.getHeaderProps(column.getSortByToggleProps())}
-										className="sticky left-0 py-4 text-left"
+										className="sticky left-0 pb-4 text-left"
 									>
 										{column.render('Header')}
 										<span>
@@ -134,7 +138,92 @@ function Table({ columns, data }) {
 	);
 }
 
-export default function Stats({ results }) {
+function SelectMenu({ people }) {
+	const [selected, setSelected] = useState(people[0]);
+	const [query, setQuery] = useState('');
+
+	const filteredPeople =
+		query === ''
+			? people
+			: people.filter((person) =>
+					person.name
+						.toLowerCase()
+						.replace(/\s+/g, '')
+						.includes(query.toLowerCase().replace(/\s+/g, ''))
+			  );
+
+	return (
+		<div className="w-72 z-10">
+			<Combobox value={selected} onChange={setSelected}>
+				<div className="relative mt-1">
+					<div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+						<Combobox.Input
+							className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+							displayValue={(person) => person.name}
+							onChange={(event) => setQuery(event.target.value)}
+						/>
+						<Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+							<SelectorIcon
+								className="h-5 w-5 text-gray-400"
+								aria-hidden="true"
+							/>
+						</Combobox.Button>
+					</div>
+					<Transition
+						as={Fragment}
+						leave="transition ease-in duration-100"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+						afterLeave={() => setQuery('')}
+					>
+						<Combobox.Options className="absolute mt-1 max-h-96 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+							{filteredPeople.length === 0 && query !== '' ? (
+								<div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+									Nothing found.
+								</div>
+							) : (
+								filteredPeople.map((person) => (
+									<Combobox.Option
+										key={person.id}
+										className={({ active }) =>
+											`relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+												active ? 'bg-red-600 text-white' : 'text-gray-900'
+											}`
+										}
+										value={person}
+									>
+										{({ selected, active }) => (
+											<>
+												<span
+													className={`block truncate ${
+														selected ? 'font-medium' : 'font-normal'
+													}`}
+												>
+													{person.name}
+												</span>
+												{selected ? (
+													<span
+														className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+															active ? 'text-white' : 'text-red-600'
+														}`}
+													>
+														<CheckIcon className="h-5 w-5" aria-hidden="true" />
+													</span>
+												) : null}
+											</>
+										)}
+									</Combobox.Option>
+								))
+							)}
+						</Combobox.Options>
+					</Transition>
+				</div>
+			</Combobox>
+		</div>
+	);
+}
+
+export default function Stats({ results, playerNameOptions }) {
 	const columns = React.useMemo(
 		() => [
 			{
@@ -174,6 +263,9 @@ export default function Stats({ results }) {
 	return (
 		<>
 			<h1 className="wrapper text-lg">Statistics</h1>
+			<div className="flex">
+				<SelectMenu people={playerNameOptions} />
+			</div>
 			<Table columns={columns} data={data} />
 		</>
 	);
@@ -186,8 +278,14 @@ export async function getStaticProps() {
 		);
 		const results = await res.json();
 
+		const playerNameOptions = [
+			...new Set(results.map((player) => player.player_name)),
+		]
+			.sort()
+			.map((string, index) => ({ id: index + 1, name: string }));
+
 		return {
-			props: { results },
+			props: { results, playerNameOptions },
 		};
 	} catch (err) {
 		console.error(err);
