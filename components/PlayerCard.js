da598eeb24555b6ccbd6a1581a942cab3cb7a1fc
Ethan Morris <ethan.morris33@gmail.com>
2022-06-23 22:47:18 -0500

    add functionality to get current team and update styles

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -6,8 +6,11 @@ export default function PlayerCard({ id }) {
 		<>
 			<Link href={`/players/${id.player_id}`}>
 				<a>
-					<div className="bg-white dark:bg-[#333333] w-fit m-4 p-4 rounded-md shadow-md hover:shadow-xl">
+					<div className="flex flex-col items-center bg-white dark:bg-[#333333] w-full p-4 rounded-md shadow-md hover:shadow-xl">
 						<div className="relative h-60 w-60">
+							<span
+								className={`block absolute inset-0 bg-[url('/logo-${id.asmc}.webp')] bg-contain opacity-20`}
+							></span>
 							<Image
 								src={`https://sleepercdn.com/content/nfl/players/${id.player_id}.jpg`}
 								alt={id.full_name}
@@ -17,12 +20,25 @@ export default function PlayerCard({ id }) {
 							></Image>
 						</div>
 						<div className="text-center">
-							<h3 className="text-xl p-4">{id.full_name}</h3>
+							<h3 className="text-xl pt-4">{id.full_name}</h3>
 							<h5>
-								{id.team}
+								{id.fantasy_positions[0]}
 								{' - '}
 								{id.number}
 							</h5>
+							<div className="relative h-8 w-8 m-2 mx-auto">
+								<Image
+									src={
+										id.team
+											? `https://sleepercdn.com/images/team_logos/nfl/${id.team.toLowerCase()}.png`
+											: `/logo.webp`
+									}
+									alt={id.full_name}
+									layout="fill"
+									objectFit="cover"
+									className=""
+								></Image>
+							</div>
 						</div>
 					</div>
 				</a>
--- pages/players/index.js
+++ pages/players/index.js
@@ -3,23 +3,76 @@ import PlayerCard from '../../components/PlayerCard';
 
 export default function Players({ results }) {
 	return (
-		<div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
-			<PlayerCard id={results[1234]} />
-			<PlayerCard id={results[4881]} />
-			<PlayerCard id={results[6813]} />
-			<PlayerCard id={results[2505]} />
-			<PlayerCard id={results[5012]} />
-			<PlayerCard id={results[6801]} />
-			<PlayerCard id={results[6151]} />
-			<PlayerCard id={results[6794]} />
-		</div>
+		<>
+			<div className="h-36 mb-6 bg-white">
+				<h1>Players</h1>
+			</div>
+			<div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
+				<PlayerCard id={results[0]} />
+				<PlayerCard id={results[1]} />
+				<PlayerCard id={results[2]} />
+				<PlayerCard id={results[3]} />
+				<PlayerCard id={results[4]} />
+				<PlayerCard id={results[5]} />
+				<PlayerCard id={results[6]} />
+				<PlayerCard id={results[7]} />
+				<PlayerCard id={results[8]} />
+				<PlayerCard id={results[9]} />
+				<PlayerCard id={results[10]} />
+				<PlayerCard id={results[11]} />
+			</div>
+		</>
 	);
 }
 
 export async function getStaticProps() {
 	try {
-		const res = await fetch('https://ethanrmorris.github.io/v1/players.json');
-		const results = await res.json();
+		const [playersRes, ownersRes, rostersRes] = await Promise.all([
+			fetch('https://ethanrmorris.github.io/v1/players.json'),
+			fetch('https://ethanrmorris.github.io/v1/owners.json'),
+			fetch('https://api.sleeper.app/v1/league/784462448236363776/rosters/'),
+		]);
+		const [players, owners, rosters] = await Promise.all([
+			playersRes.json(),
+			ownersRes.json(),
+			rostersRes.json(),
+		]);
+
+		const newResults = Object.values(players);
+
+		const playerCards = [
+			'6813',
+			'7528',
+			'6790',
+			'4984',
+			'6786',
+			'4866',
+			'6797',
+			'4040',
+			'7596',
+			'7564',
+			'7553',
+			'6945',
+		];
+
+		const newerResults = newResults.filter((item) =>
+			playerCards.includes(item.player_id)
+		);
+
+		const results = newerResults.map((player) => {
+			const id = player.player_id;
+			const currentTeam = rosters.find((team) => team.players.includes(id));
+
+			const currentOwner = currentTeam?.roster_id;
+
+			const cleanOwner = owners.find((owner) =>
+				owner.id?.includes(currentOwner)
+			);
+
+			const ownerName = cleanOwner?.slug;
+
+			return { ...player, asmc: ownerName };
+		});
 
 		return {
 			props: { results },
