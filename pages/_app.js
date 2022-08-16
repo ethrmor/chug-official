bbb6468eff399d83607289cc06d536c5a3e53e6d
Ethan Morris <ethan.morris33@gmail.com>
2022-08-16 12:58:51 -0500

    fix favicon & load issue with call to supabase for trades

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,15 @@ import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider attribute="class">
 			<Layout>
+				<Head>
+					<link rel="icon" href="/favicon.webp" />
+				</Head>
 				<Component {...pageProps} />
 			</Layout>
 		</ThemeProvider>
